036192ef0630c77e708dd913f1bb1b5c10053991
robertwoodward <robertjwoodward@outlook.com>
2024-03-24 13:34:20 +1000

    Complete main func

--- FSync/Program.fs
+++ FSync/Program.fs
@@ -10,7 +10,11 @@ open FSync.CopyFiles
 let main argv =
     match argv with
     | [| sourceDirPathArg; targetDirPathArg |] ->
-        0 
+        deleteExtraDirs targetDirPathArg sourceDirPathArg
+        deleteExtraFiles targetDirPathArg sourceDirPathArg
+        createMissingDirs sourceDirPathArg targetDirPathArg
+        copyMissingFiles sourceDirPathArg targetDirPathArg
+        0
     | _ ->
         printError "Provide a source and target directory path"
         1
